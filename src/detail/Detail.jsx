faf77c7aed2d79a0ff2bc2d51f2a521f3c9463b9
BlossomS2 <rose970515@naver.com>
2024-08-30 10:50:13 +0900

    detail_post_test

---
+++ src/detail/Detail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post';
+import Comment from './Comment';
+
+function Detail() {
+  return (
+    <content>
+      <Post />
+      <Comment />
+    </content>
+  );
+}
+
+export default Detail;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MyPage from '../components/mypage/Mypage';
+import Detail from '../detail/Detail';
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
         <Routes>
           <Route path="/" element={<div>예시</div>} />
           <Route path="/mypage" element={<MyPage />} />
+          <Route path="/detail" element={<Detail />} />
         </Routes>
       </BrowserRouter>
     </>
