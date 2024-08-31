e01d45e61dfadb23562b826aeed92071a6109732
sodol-dotcom <sojeong0521@gmail.com>
2024-08-31 16:06:16 +0900

    ✨ Feat: AuthPages 및 WritePage 라우팅 추가
    - SignIn.jsx와 SignUp.jsx를 src/components/AuthPages 폴더에 추가
    - Write.jsx를 src/components/AuthPages 폴더에 추가
    - shared/Router.jsx에 새 페이지들에 대한 라우트 설정 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MyPage from '../components/mypage/Mypage';
-import Detail from '../detail/Detail';
-import MainPage from '../components/main/MainPage';
 import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
+import MainPage from '../components/main/MainPage';
+import SignIn from '../components/AuthPages/SignIn';
+import SignUp from '../components/AuthPages/SignUp';
+import MyPage from '../components/mypage/Mypage';
+import Detail from '../detail/Detail';
+import Write from '../components/WritePage/Write';
 
 export default function Router() {
   return (
@@ -15,8 +18,8 @@ export default function Router() {
           <Route path="/" element={<SignIn />} />
           <Route path="/" element={<SignUp />} />
           <Route path="/mypage" element={<MyPage />} />
-          <Route path="/write" element={<Write />} />
           <Route path="/detail" element={<Detail />} />
+          <Route path="/write" element={<Write />} />
         </Routes>
       </BrowserRouter>
       <Footer />
